84526832254c36cf27cc2ad1cc688cac8af0230f
sarkar <sahilverma1@gmail.com>
2014-07-14 16:18:39 -0700

    Code Cleanup

--- ios-samples/SwiftSimpleSample/SwiftSimpleSample/SampleViewController.swift
+++ ios-samples/SwiftSimpleSample/SwiftSimpleSample/SampleViewController.swift
@@ -3,8 +3,7 @@
     See LICENSE.txt for this sample’s licensing information
     
     Abstract:
-    
-                A view controller that demonstrates how to use UITextField.
+    A view controller that demonstrates how to use UITextField.
             
 */
 
@@ -39,30 +38,23 @@ class SampleViewController: UIViewController, UITextFieldDelegate {
     // MARK: UITextFieldDelegate
     
     func onClick(sender:AnyObject!) {
-        // Save moment
-        // http://docs.kiip.com/en/guide/android.html#getting_rewards
+
+        // http://docs.kiip.com/en/guide/ios.html#getting_rewards
         if sender.isEqual(self.saveMoment) {
             self.dismissKeyboard()
     
             var momentId:String = self.momentIdField.text
             var momentValue:NSString = self.momentValueField.text
 
-            Kiip.sharedInstance().saveMoment(momentId, value:momentValue.doubleValue, withCompletionHandler:{(poptart:KPPoptart!, error:NSError!) -> Void in
-                    if error {
-                        self.showError(error)
-                    }
-
-                    // Since we've implemented this block, Kiip will no longer show the poptart automatically
-                    // WARNING: poptart may be nil if no reward was given
+            Kiip.sharedInstance().saveMoment(momentId, value:momentValue.doubleValue, withCompletionHandler: {(poptart:KPPoptart!, error:NSError!) -> Void in
+                if error {
+                    self.showError(error)
+                }
+                if poptart {
                     poptart.show()
-                })
+                }
+            })
         }
-    }
-    
-    func showError(error:NSError) {
-        var alert = UIAlertController(title:"Error", message:error.localizedDescription, preferredStyle: UIAlertControllerStyle.Alert)
-        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
-        self.presentViewController(alert, animated: true, completion: nil)
     }
     
     func textFieldShouldReturn(textField:UITextField) -> Bool  {
@@ -72,12 +64,20 @@ class SampleViewController: UIViewController, UITextFieldDelegate {
         else if self.momentValueField == textField {
             textField.resignFirstResponder()
         }
-    
+
         return false
     }
     
     func dismissKeyboard() {
         self.momentIdField.resignFirstResponder()
         self.momentValueField.resignFirstResponder()
+    }
+
+    // MARK: Kiip Error handler
+
+    func showError(error:NSError) {
+        var alert = UIAlertController(title:"Error", message:error.localizedDescription, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
     }
 }
