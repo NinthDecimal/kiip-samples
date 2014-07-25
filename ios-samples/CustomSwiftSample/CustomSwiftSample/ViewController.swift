0fe1267080325e2a6def719f69d6d48e82d81e38
sarkar <sahilverma1@gmail.com>
2014-07-25 12:00:08 -0700

    Add PickerView

--- ios-samples/CustomSwiftSample/CustomSwiftSample/ViewController.swift
+++ ios-samples/CustomSwiftSample/CustomSwiftSample/ViewController.swift
@@ -8,11 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    let kRewardDefault = "Default Reward Experience"
+    let kNotificationCustom = "Custom Notification"
+    let kNotificationIntegrated = "Integrated Notifcation"
+    let kPickerViewComponents = 1
+
+    var rewardFlowType : String!
+    var pickerDataSource : Array<String>!
+
+    @IBOutlet weak var navBar: UINavigationBar!
+    @IBOutlet weak var scrollPicker: UIPickerView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        navBar.topItem.title = "Kiip SDK \(KPVersion)"
+
+        // Set picker view & default reward type
+        pickerDataSource = [kRewardDefault,
+                            kNotificationCustom,
+                            kNotificationIntegrated]
+        rewardFlowType = kRewardDefault
+        scrollPicker.dataSource = self
+        scrollPicker.delegate = self
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +41,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: - UIPickerViewDataSource
+    func numberOfComponentsInPickerView(pV:UIPickerView!) -> Int {
+        return kPickerViewComponents
+    }
+
+    func pickerView( pV:UIPickerView!,
+        numberOfRowsInComponent component: Int) -> Int {
+        return pickerDataSource.count
+    }
+
+    //MARK: - UIPickerViewDelegate
+    func pickerView(pickerView: UIPickerView!, titleForRow row: Int, forComponent component: Int) -> String! {
+        return pickerDataSource[row]
+    }
+
+    func pickerView(pickerView:UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        rewardFlowType = pickerDataSource[row]
+    }
 
 }
-
