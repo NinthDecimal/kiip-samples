c79b4b2d12768733ebdeeb842b6de0ace3a78a92
Shane <shaneRudolfWorktive@gmail.com>
2016-11-06 21:55:38 -0800

    add start session and end session

--- corona-samples/SpaceShooter/Corona/main.lua
+++ corona-samples/SpaceShooter/Corona/main.lua
@@ -16,3 +16,39 @@ audio.setVolume( 0.5, { channel=1 } )
 
 -- Go to the menu screen
 composer.gotoScene( "menu" )
+
+local function startSession()
+    kiip.startSession({
+        onSessionStarted = function()
+            print("KiipPlugin, lua onSessionStarted ")
+        end,
+        onSessionStartFailed = function(errMsg)
+            print("KiipPlugin, lua onSessionStartFailed errMsg = ".. errMsg)
+        end
+    })
+end
+
+local function endSession()
+    kiip.endSession({
+        onSessionEnded = function()
+            print("KiipPlugin, lua onSessionEnded ")
+        end,
+        onSessionEndFailed = function(errMsg)
+            print("KiipPlugin, lua onSessionEndFailed errMsg = ".. errMsg)
+        end
+    })
+end
+
+local function onSystemEvent( event )
+	if event.type == "applicationSuspend" then
+		print( "KiipPlugin, end session" )
+		endSession()
+	elseif event.type == "applicationResume" then
+			print( "KiipPlugin, start session" )
+		startSession()
+	end
+	
+    print( "KiipPlugin, System event name and type: " .. event.name, event.type )
+end
+
+Runtime:addEventListener( "system", onSystemEvent )
